482ecc377864ae0be86416a4d47a085ba0608ed6
Alperen <alperen@eduroam-int-dhcp-97-81-111.ucl.ac.uk>
2018-06-22 11:24:18 +0100

    Adding functionality to loop over multiple files

--- ImageRecognition/tf-pose-estimation/run.py
+++ ImageRecognition/tf-pose-estimation/run.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import sys
 import time
+import os
 
 from tf_pose import common
 import cv2
@@ -27,6 +28,7 @@ if __name__ == '__main__':
                         help='if provided, resize images before they are processed. default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
     parser.add_argument('--resize-out-ratio', type=float, default=4.0,
                         help='if provided, resize heatmaps before they are post-processed. default=1.0')
+    parser.add_argument('--directory', type=str, default='./images/', help='')
 
     args = parser.parse_args()
 
@@ -37,6 +39,8 @@ if __name__ == '__main__':
         e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
 
     # estimate human poses from a single image !
+    print("args image")
+    print(args.image)
     image = common.read_imgfile(args.image, None, None)
     if image is None:
         logger.error('Image can not be read, path=%s' % args.image)
@@ -49,43 +53,51 @@ if __name__ == '__main__':
 
     image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
 
-    print(len(humans))
-    #for human in humans:
+    directory = os.fsencode(args.directory)
 
-    #print(e)
-    import matplotlib.pyplot as plt
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".jpg"):
+            dir = "./images/" + filename
+            image = common.read_imgfile(dir, None, None)
+            if image is None:
+                logger.error('Image can not be read, path=%s' % args.image)
+                continue
+            t = time.time()
+            humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
+            elapsed = time.time() - t
+            print(humans)
 
-    fig = plt.figure()
-    a = fig.add_subplot(2, 2, 1)
-    a.set_title('Result')
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-    bgimg = cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_BGR2RGB)
-    bgimg = cv2.resize(bgimg, (e.heatMat.shape[1], e.heatMat.shape[0]), interpolation=cv2.INTER_AREA)
-
-    # show network output
-    a = fig.add_subplot(2, 2, 2)
-    plt.imshow(bgimg, alpha=0.5)
-    tmp = np.amax(e.heatMat[:, :, :-1], axis=2)
-    plt.imshow(tmp, cmap=plt.cm.gray, alpha=0.5)
-    plt.colorbar()
-
-    tmp2 = e.pafMat.transpose((2, 0, 1))
-    tmp2_odd = np.amax(np.absolute(tmp2[::2, :, :]), axis=0)
-    tmp2_even = np.amax(np.absolute(tmp2[1::2, :, :]), axis=0)
-
-    #print(tmp2)
-    #print(tmp2_odd)
-    #print(tmp2_even)
-    a = fig.add_subplot(2, 2, 3)
-    a.set_title('Vectormap-x')
-    # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
-    plt.imshow(tmp2_odd, cmap=plt.cm.gray, alpha=0.5)
-    plt.colorbar()
-
-    a = fig.add_subplot(2, 2, 4)
-    a.set_title('Vectormap-y')
-    # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
-    plt.imshow(tmp2_even, cmap=plt.cm.gray, alpha=0.5)
-    plt.colorbar()
-    plt.show()
+    # import matplotlib.pyplot as plt
+    #
+    # fig = plt.figure()
+    # a = fig.add_subplot(2, 2, 1)
+    # a.set_title('Result')
+    # plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    #
+    # bgimg = cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_BGR2RGB)
+    # bgimg = cv2.resize(bgimg, (e.heatMat.shape[1], e.heatMat.shape[0]), interpolation=cv2.INTER_AREA)
+    #
+    # # show network output
+    # a = fig.add_subplot(2, 2, 2)
+    # plt.imshow(bgimg, alpha=0.5)
+    # tmp = np.amax(e.heatMat[:, :, :-1], axis=2)
+    # plt.imshow(tmp, cmap=plt.cm.gray, alpha=0.5)
+    # plt.colorbar()
+    #
+    # tmp2 = e.pafMat.transpose((2, 0, 1))
+    # tmp2_odd = np.amax(np.absolute(tmp2[::2, :, :]), axis=0)
+    # tmp2_even = np.amax(np.absolute(tmp2[1::2, :, :]), axis=0)
+    #
+    # a = fig.add_subplot(2, 2, 3)
+    # a.set_title('Vectormap-x')
+    # # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
+    # plt.imshow(tmp2_odd, cmap=plt.cm.gray, alpha=0.5)
+    # plt.colorbar()
+    #
+    # a = fig.add_subplot(2, 2, 4)
+    # a.set_title('Vectormap-y')
+    # # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
+    # plt.imshow(tmp2_even, cmap=plt.cm.gray, alpha=0.5)
+    # plt.colorbar()
+    # plt.show()
