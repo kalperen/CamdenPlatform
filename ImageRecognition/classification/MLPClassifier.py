e05034b2510321e342dc59d5eb8a57a56fcf9085
Alperen Karaoglu <alperenkaraoglu@gmail.com>
2018-06-30 20:38:02 +0100

    Removing images, adding extracted feature datasets, updating readme, tuning classifier, current accuracy 84%

--- ImageRecognition/classification/MLPClassifier.py
+++ ImageRecognition/classification/MLPClassifier.py
@@ -3,7 +3,8 @@ import pandas as pd
 from sklearn.neural_network import MLPClassifier
 
 X_train, y_train, X_valid, y_valid, train_data, validation_data = get_data()
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(8, 25), random_state=1, activation='logistic')
 clf.fit(X_train, y_train)
 print(clf.predict(X_valid))
-print(clf.score(X_valid, y_valid))
+print("\nACCURACY:")
+print(str(clf.score(X_valid, y_valid)*100) + "%")
