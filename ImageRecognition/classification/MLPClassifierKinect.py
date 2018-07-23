af856f001b0ede8414f8fe9a2ef8989f872f5be6
Shahroz Ahmed <shahroz.ahm@gmail.com>
2018-07-23 14:39:01 +0100

    Enables file to run constantly run the classifier from dotNet and output results

---
+++ ImageRecognition/classification/MLPClassifierKinect.py
@@ -0,0 +1,18 @@
+import os
+
+from kinect_data_processing import get_data
+from sklearn.neural_network import MLPClassifier
+
+X_train, y_train = get_data()
+
+folder = os.path.normpath(os.getcwd() + os.sep + os.pardir + os.sep + os.pardir + os.sep
+                          + os.pardir + os.sep + os.pardir + os.sep + os.pardir + os.sep + os.pardir)
+csv_dir = os.path.normpath(folder + '/Datasets/kinect_output.csv')
+
+X_test, y_test = get_data(data_dir=csv_dir)
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(8, 16), random_state=1,
+                    activation='logistic')
+clf.fit(X_train, y_train)
+print("The output results: ")
+x = clf.predict(X_test)
+print(x)
