6895ba8e8a4b3bd4a25d1c7fb903ce3ec7f06591
Alperen Karaoglu <alperenkaraoglu@gmail.com>
2018-08-13 11:31:41 +0100

    refactoring and documenting classifiers

--- ImageRecognition/classification/MLPClassifierKinect.py
+++ ImageRecognition/classification/MLPClassifierKinect.py
@@ -10,9 +10,13 @@ folder = os.path.normpath(os.getcwd() + os.sep + os.pardir + os.sep + os.pardir 
 csv_dir = os.path.normpath(folder + '/Datasets/kinect_output.csv')
 
 X_test, y_test = get_data(data_dir=csv_dir)
+
+#Define the classifier
 clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(8, 16), random_state=1,
                     activation='logistic')
+#Fit the classifier
 clf.fit(X_train, y_train)
+#Print out results
 print("The output results: ")
 x = clf.predict(X_test)
 print(x)
