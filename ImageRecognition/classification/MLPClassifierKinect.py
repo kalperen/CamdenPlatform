93993d3753343f1f1141d58b49e1c5a3fe08eafa
Shahroz Ahmed <shahroz.ahm@gmail.com>
2018-08-13 11:47:12 +0100

    kinect data processing clean up

--- ImageRecognition/classification/MLPClassifierKinect.py
+++ ImageRecognition/classification/MLPClassifierKinect.py
@@ -1,18 +1,24 @@
 import os
-
 from kinect_data_processing import get_data
 from sklearn.neural_network import MLPClassifier
 
-X_train, y_train = get_data()
-
+# path of kinect output file
 folder = os.path.normpath(os.getcwd() + os.sep + os.pardir + os.sep + os.pardir + os.sep
-                          + os.pardir + os.sep + os.pardir + os.sep + os.pardir + os.sep + os.pardir)
-csv_dir = os.path.normpath(folder + '/Datasets/kinect_output.csv')
+                              + os.pardir + os.sep + os.pardir + os.sep + os.pardir + os.sep + os.pardir)
+dir = os.path.normpath(folder + '/Datasets/kinect_output.csv')
 
-X_test, y_test = get_data(data_dir=csv_dir)
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(8, 16), random_state=1,
-                    activation='logistic')
-clf.fit(X_train, y_train)
-print("The output results: ")
-x = clf.predict(X_test)
-print(x)
+
+def run(x, y, csv_dir=dir):
+
+    X_test, y_test = get_data(data_dir=csv_dir)
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(8, 16), random_state=1,
+                        activation='logistic')
+    clf.fit(x, y)
+    print("The output results: ")
+    x = clf.predict(X_test)
+    print(x)
+
+
+if __name__ == '__main__':
+    X_train, y_train = get_data()
+    run(X_train, y_train)
